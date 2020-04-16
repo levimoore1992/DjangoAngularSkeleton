d7d63bf021eccc8a7c9662ce35c085f291757662
levimoore1992 <levi.moore@exxonmobil.com>
2020-04-15 19:11:24 -0500

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders.middleware.CorsMiddleware',
+
 
 ]
 CORS_ORIGIN_ALLOW_ALL = True
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
