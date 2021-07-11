aef564961eef1094e28fe78fbd7f79578ff492e2
levimoore1992 <levi.moore@exxonmobil.com>
2021-07-11 15:07:34 -0400

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,8 +1,14 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/rest-auth/", include("rest_auth.urls")),
     path("api/rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
